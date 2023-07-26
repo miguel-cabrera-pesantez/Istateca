12a7c271b0272695756901e1de8e4e3fa2af4e67
miguel-cabrera-pesantez <97999904+miguel-cabrera-pesantez@users.noreply.github.com>
2023-07-25 21:02:17 -0500

    etiquetas  borrar

--- src/main/java/com/istateca/app/istateca/controllers/EtiquetaLibroController.java
+++ src/main/java/com/istateca/app/istateca/controllers/EtiquetaLibroController.java
@@ -1,11 +1,14 @@
 package com.istateca.app.istateca.controllers;
 
 import com.istateca.app.istateca.models.EtiquetaLibro;
+import com.istateca.app.istateca.models.Libro;
 import com.istateca.app.istateca.services.BaseService;
 import com.istateca.app.istateca.services.EtiquetaLibroService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tags")
@@ -26,4 +29,21 @@ public class EtiquetaLibroController extends BaseController<EtiquetaLibro> {
     protected BaseService<EtiquetaLibro, Integer> getService() {
         return service;
     }
+
+    @DeleteMapping("/eliminaretiqueta")
+    public ResponseEntity<String> librosxCoincidencia(@RequestParam(value = "parametro") Integer parametro) {
+        if (service.borrarEtiquetaLibro(parametro))return ResponseEntity.ok("Borrado "+parametro);
+        else return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/etiquetasxlibro")
+    public ResponseEntity<List<EtiquetaLibro>> etiquetasxlibro(@RequestParam(value = "parametro") Integer parametro) {
+        List<EtiquetaLibro> etiquetaLibros = service.etiquetasxLibro(parametro);
+        if (etiquetaLibros.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(etiquetaLibros);
+        }
+    }
+
 }
--- src/main/java/com/istateca/app/istateca/services/EtiquetaLibroImpl.java
+++ src/main/java/com/istateca/app/istateca/services/EtiquetaLibroImpl.java
@@ -6,6 +6,8 @@ import com.istateca.app.istateca.models.EtiquetaLibro;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EtiquetaLibroImpl extends BaseServiceImpl<EtiquetaLibro, Integer> implements EtiquetaLibroService {
 
@@ -17,4 +19,20 @@ public class EtiquetaLibroImpl extends BaseServiceImpl<EtiquetaLibro, Integer> i
     }
 
 
+    @Override
+    public List<EtiquetaLibro> etiquetasxLibro(Integer id) {
+        return repository.findByLibroId(id);
+    }
+
+    @Override
+    public boolean borrarEtiquetaLibro(Integer id) {
+        try {
+            repository.deleteById(id);
+            return true;
+        }catch (Exception e){
+            System.out.println(e);
+            return false;
+        }
+
+    }
 }
